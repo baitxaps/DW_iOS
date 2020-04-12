ae4c2feeeb670dc3127f190b9483c35e618cc1ba
hairong chen <chenhairong@itouchtv.cn>
2020-04-12 20:40:23 +0800

    update lib

--- DW_iOS/Weibo/View/Main/WeiBoMainViewController.swift
+++ DW_iOS/Weibo/View/Main/WeiBoMainViewController.swift
@@ -61,7 +61,7 @@ extension WeiBoMainViewController {
     private func addChild() {
         addChild(vc: WBHomeTableViewController(),title: "首页",imageName: "tabbar_home")
         addChild(vc: PicturePickerController(),title: "消息",imageName: "tabbar_message_center")
-//        addChild(vc: MessageTableViewController(),title: "消息",imageName: "tabbar_message_center")
+//       addChild(vc: MessageTableViewController(),title: "消息",imageName: "tabbar_message_center")
      // addChild(vc: UIViewController(),title:"" ,imageName:"")
         addChild(UIViewController())
         addChild(vc: WBDiscoverTableViewController(),title: "发现",imageName: "tabbar_discover")
